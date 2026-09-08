967867e2b105c5e99754e86eccf6c1bb2877245e
arthur.lima3@icloud.com <arthur.lima3@icloud.com>
2026-09-08 19:10:03 -0300

    feat: finalizando seção 07

---
+++ src/secao07_topicos_basicos/funcoes_strings/Program.java
@@ -0,0 +1,15 @@
+package secao07_topicos_basicos.funcoes_strings;
+
+public class Program {
+    public static void main(String[] args) {
+        String s = "potato apple lemon orange";
+
+        String[] vect = s.split(" ");
+
+        System.out.println(vect[0]);
+        System.out.println(vect[1]);
+        System.out.println(vect[2]);
+        System.out.println(vect[3]);
+    }
+
+}
