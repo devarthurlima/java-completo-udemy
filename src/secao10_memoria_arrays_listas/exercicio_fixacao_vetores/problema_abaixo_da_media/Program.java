35f891ba1c0f1fbb11f9fb9570c3b91755b95dfe
arthur.lima3@icloud.com <arthur.lima3@icloud.com>
2026-09-16 20:45:57 -0300

    feat: continuação dos exercicios de vetores.

---
+++ src/secao10_memoria_arrays_listas/exercicio_fixacao_vetores/problema_abaixo_da_media/Program.java
@@ -0,0 +1,41 @@
+package secao10_memoria_arrays_listas.exercicio_fixacao_vetores.problema_abaixo_da_media;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        double soma, media;
+
+        System.out.print("Quantos elementos vai ter o vetor? ");
+        n = sc.nextInt();
+
+        double[] vetor = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite um número: ");
+            vetor[i] = sc.nextDouble();
+        }
+
+        soma = 0;
+        for (int i = 0; i < n; i++) {
+            soma = soma + vetor[i];
+        }
+
+        media = soma / n;
+
+        System.out.printf("\nMédia do vetor = %.3f\n", media);
+
+        System.out.printf("Elementos abaixo da média: ");
+        for (int i = 0; i < n; i++) {
+            if (vetor[i] < media) {
+                System.out.printf("\n%.1f", vetor[i]);
+            }
+        }
+        sc.close();
+    }
+}
---
+++ src/secao10_memoria_arrays_listas/exercicio_fixacao_vetores/problema_soma_vetores/Program.java
@@ -0,0 +1,39 @@
+package secao10_memoria_arrays_listas.exercicio_fixacao_vetores.problema_soma_vetores;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+
+        System.out.print("Quantos valores vai ter cada setor? ");
+        n = sc.nextInt();
+
+        int[] a = new int[n];
+        int[] b = new int[n];
+        int[] c = new int[n];
+
+        System.out.println("Digite os valores de A: ");
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println("Digite os valores de B: ");
+        for (int i = 0; i < n; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            c[i] = a[i] + b[i];
+        }
+
+        System.out.println("Vetor Resultante");
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%d\n", c[i]);
+        }
+
+        sc.close();
+    }
+}
