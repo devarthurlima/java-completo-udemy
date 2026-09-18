7c91d13ac3dc98ec80ce2be2d7384fb269fa75cd
arthur.lima3@icloud.com <arthur.lima3@icloud.com>
2026-09-18 19:52:48 -0300

    feat: Inicinado for each.

---
+++ src/secao10_memoria_arrays_listas/for_each/application/Program.java
@@ -0,0 +1,16 @@
+package secao10_memoria_arrays_listas.for_each.application;
+
+public class Program {
+    public static void main(String[] args) {
+        String[] vect = new String[] { "Maria", "Bob", "Alex" };
+        for (int i = 0; i < vect.length; i++) {
+            System.out.println(vect[i]);
+        }
+
+        System.out.println("-----------------------");
+
+        for (String obj : vect) {
+            System.out.println(obj);
+        }
+    }
+}
